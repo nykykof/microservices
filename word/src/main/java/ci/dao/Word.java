bf6000efab64599ef10a04992ca761608a1525bd
nykykof <nykykof@gmail.com>
2017-05-16 18:07:12 +0200

    à deployer sous docker

---
+++ word/src/main/java/ci/dao/Word.java
@@ -0,0 +1,28 @@
+package ci.dao;
+
+
+public class Word {
+	
+	private String word;
+
+	public Word() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Word(String word) {
+		super();
+		this.word = word;
+	}
+
+	public String getWord() {
+		return word;
+	}
+
+	public void setWord(String word) {
+		this.word = word;
+	}
+	
+	
+
+}
